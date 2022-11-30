ad3c017432de814dc63f29de432c06200dfc1d82
kongkiat.b <kongkiat.b@komgrip.com>
2022-12-01 02:07:48 +0700

    Test

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ require("core/config.core.php");
 require("core/connect.core.php");
 $getdata = new clear_db();
 $connect = $getdata->my_sql_connect(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
-mysqli_set_charset($connect, "utf8");
+mysqli_set_charset($connect, "utf8"); 
 // $getgroup_product = $getdata->my_sql_query($connect, null, 'group_type_product', null);
  ?>
 <!doctype html>
